578b137b191e07b49230140484cdddc4d80f1bd8
Ingvildhaha <ingvild.bg@hotmail.com>
2026-03-17 14:48:40 +0100

    Booking, fake api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Button from './components/Button'
-import ApiExample from './components/ApiExample'; 
+import ApiExample from './components/ApiExample'
+import BookingList from './components/BookingList'; 
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
       <p>Under ser du et enkelt eksempel på et API-kall:</p>
 
       <ApiExample />
+
+       <p>Under ser du en enkel liste med "bookinger" hentet fra et API:</p>
+      <BookingList />
     </div>
   )
 }
